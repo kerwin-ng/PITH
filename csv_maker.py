6ba0ea6d68a923a8abb53e6e5be48bffa76ed8fd
Kerwin <palewhx@gmail.com>
2022-07-17 23:22:57 +0800

    一些小修小补

--- csv_maker.py
+++ csv_maker.py
@@ -1,17 +1,20 @@
+# Version: 0.2
+# Date: 2022/07/16
+
 import csv
 import os
 
 
 def config_csv(csv_path, tbp):
-    header = ['path', 'name', 'time_text', 'location_text', 'status']
+    header = ['path', 'name', 'time_text', 'location_text']
     with open(csv_path, 'w+', encoding='utf-8-sig', newline='') as file_obj:
         writer = csv.writer(file_obj)
         writer.writerow(header)
         for root, ds, fs in os.walk(tbp):
             for f in fs:
                 path = os.path.join(root, f)
-                data = (path, f, '', '', 'No')
-                print(data)
+                data = (path, f, '', '', '')
+                print('正在写入[{}]' .format(data))
                 writer.writerow(data)
 
         print('Done')
